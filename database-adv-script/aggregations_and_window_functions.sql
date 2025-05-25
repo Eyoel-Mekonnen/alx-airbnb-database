5de4305489f4ec6094dfb604c4dbf87d15d8ef4f
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2025-05-25 17:05:20 +0300

    ROW_NUMBER

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -11,7 +11,7 @@ SELECT
     Property.property_id,
     Property.name,
     COUNT(Booking.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(Booking.booking_id) DESC) AS booking_rank
+    ROW_NUMBER() OVER (ORDER BY COUNT(Booking.booking_id) DESC) AS booking_rank
 FROM Property
 LEFT JOIN Booking ON Property.property_id = Booking.property_id
 GROUP BY Property.property_id, Property.name
