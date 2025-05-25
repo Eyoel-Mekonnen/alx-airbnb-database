9d20f723b7c6b13eae585ff8580e5f5d167b04aa
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2025-05-25 14:44:17 +0300

    full_outer_join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -8,4 +8,20 @@ INNER JOIN User ON Booking.user_id = User.user_id;
 
 
 
--- 
+
+
+
+--  Left Join => Property Left table
+
+SELECT Property.property_id, Property.host_id, Property.name, Property.description, Property.location, Property.pricepernight, Review.review_id, Review.user_id, Review.rating, Review.comment
+FROM Property
+LEFT JOIN Review On Property.property_id = Review.property_id;
+
+
+
+-- FULL OUTER JOIN User and booking
+
+SELECT User.user_id, User.first_name, User.last_name, User.email, User.phone_number, User.role,
+Booking.property_id, Booking.start_date, Booking.end_date, Booking.status
+FROM User
+FULL OUTER JOIN Booking ON User.user_id = Booking.user_id;
