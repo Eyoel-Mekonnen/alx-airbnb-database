1d95a727033cd0a3babbbe725536be6e66710f6a
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2025-05-25 15:41:50 +0300

    sub query

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,21 @@
+-- create sub query
+
+
+SELECT Property.property_id, Property.name, Property.location, Property.pricepernight
+FROM Property
+WHERE Property.property_id IN (
+    SELECT Review.property_id
+    FROM Review
+    GROUP BY Review.property_id
+    HAVING AVG(Review.rating) > 4.0
+);
+
+
+
+SELECT User.user_id, User.first_name, User.last_name, User.email 
+FROM User
+WHERE (
+    SELECT COUNT(*)
+    FROM Booking
+    WHERE Booking.user_id = User.user_id
+) > 3;
