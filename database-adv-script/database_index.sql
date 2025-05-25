908d0c9d43a1faba50f9d137ca029c6a6a1819af
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2025-05-25 19:25:01 +0300

    index

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,11 +1,17 @@
--- Indexes to improve JOIN and WHERE clause performance
+-- Create indexes to optimize performance
 
 -- Booking table
 CREATE INDEX idx_booking_user_id ON Booking(user_id);
 CREATE INDEX idx_booking_property_id ON Booking(property_id);
-CREATE INDEX idx_booking_status ON Booking(status);
 
 -- Property table
 CREATE INDEX idx_property_location ON Property(location);
-CREATE INDEX idx_property_pricepernight ON Property(pricepernight);
+
+-- Measure performance with EXPLAIN
+EXPLAIN SELECT * FROM Booking WHERE user_id = 'sample-user-id';
+
+EXPLAIN SELECT Property.name, COUNT(*)
+FROM Property
+JOIN Booking ON Property.property_id = Booking.property_id
+GROUP BY Property.property_id;
 
