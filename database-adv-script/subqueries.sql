a3643ba37b945f693468a2e6f3827599da46d88a
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2025-05-25 16:55:39 +0300

    subqury

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,21 +1,23 @@
 -- create sub query
 
 
-SELECT Property.property_id, Property.name, Property.location, Property.pricepernight
+SELECT Property.name, Property.property_id, Property.host_id, Property.description,
+Property.location, Property.pricepernight
 FROM Property
 WHERE Property.property_id IN (
     SELECT Review.property_id
     FROM Review
     GROUP BY Review.property_id
-    HAVING AVG(Review.rating) > 4.0
+    HAVING AVG(Review.rating) > 4
 );
 
+-- the outer qury is iterating on each row the command with is is being executed
 
-
-SELECT User.user_id, User.first_name, User.last_name, User.email 
+SELECT User.user_id, User.first_name, User.last_name, User.email
 FROM User
 WHERE (
     SELECT COUNT(*)
     FROM Booking
     WHERE Booking.user_id = User.user_id
 ) > 3;
+
