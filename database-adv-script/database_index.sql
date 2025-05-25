029454f703b287159efeab6d47c4cbe0b0573fe1
Eyoel-Mekonnen <eyoelakatommyshellby@gmail.com>
2025-05-25 19:27:06 +0300

    included indexing

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,16 +1,15 @@
--- Create indexes to optimize performance
-
--- Booking table
+-- Create indexes
+CREATE INDEX idx_user_email ON User(email);
 CREATE INDEX idx_booking_user_id ON Booking(user_id);
 CREATE INDEX idx_booking_property_id ON Booking(property_id);
-
--- Property table
 CREATE INDEX idx_property_location ON Property(location);
 
--- Measure performance with EXPLAIN
-EXPLAIN SELECT * FROM Booking WHERE user_id = 'sample-user-id';
+-- Measure performance using EXPLAIN ANALYZE
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE user_id = 'some-user-id';
 
-EXPLAIN SELECT Property.name, COUNT(*)
+EXPLAIN ANALYZE
+SELECT Property.property_id, COUNT(*) AS booking_count
 FROM Property
 JOIN Booking ON Property.property_id = Booking.property_id
 GROUP BY Property.property_id;
